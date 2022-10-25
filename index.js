a84e381c2e993ff10a5fdcbc0a950d0355adb06d
Bega-pixel <bega2000@protonmail.com>
2022-10-25 14:54:09 +0200

    ejercico4

---
+++ index.js
@@ -0,0 +1,10 @@
+const nombre = 'Eduard';
+const apellido = 'Eduawrds'
+const estudiante = `${nombre} ${apellido}`;
+const estudianteMayus = estudiante.toLocaleUpperCase();
+const estudianteMinus = estudiante.toLocaleLowerCase;
+const numLetras = estudiante.length();
+const primNom = nombre[0];
+const ultiNom = apellido[-1];
+const espacioEstudiante = estudiante.join();
+const estudianteNombre = estudiante.includes(nombre);
